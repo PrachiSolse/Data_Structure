597da2f156461fbdadbd319e1f74292ae073c26e
PrachiSolse <prachi18st@gmail.com>
2026-05-25 22:35:24 +0530

    Reverse list

--- List_1.py
+++ List_1.py
@@ -6,3 +6,13 @@ rev=[]
 for i in range(len(original)-1,-1,-1):
     rev.append(original[i])
 print(rev)
+
+# Method 2:
+lst=[1,2,3,4,5]
+start=0
+end=len(lst)-1
+while start<end:
+    lst[start],lst[end]=lst[end],lst[start]
+    start+=1
+    end-=1
+print(lst)
